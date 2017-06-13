93fc3784cec686debc0aa14f9b985b7e1c0d8612
Luke Ghenco <lukeghenco@gmail.com>
2017-06-13 14:02:54 -0400

    Update PetsShowTest params

--- test/PetsShowTest.js
+++ test/PetsShowTest.js
@@ -8,14 +8,14 @@ import PetsShow from '../src/containers/PetsShow';
 describe('PetsShow', () => {
 
   it('renders the pets show component', () => {
-    const matchParams = { params: { movieId: 1 } }
+    const matchParams = { params: { petId: 1 } }
     const wrapper = mount(<FakeProvider>< PetsShow match={matchParams} /></FakeProvider>);
     expect(wrapper.find('h2').length).toEqual(1, 'Pets show should have an h2 with the pets name');
     expect(wrapper.find('h2').text()).toEqual('Grover');
   })
 
   it('finds the pet by the route ID', () => {
-    const matchParams = { params: { movieId: 2 } }
+    const matchParams = { params: { petId: 2 } }
     const wrapper = mount(<FakeProvider>< PetsShow match={matchParams} /></FakeProvider>);
     expect(wrapper.find('h2').length).toEqual(1, 'The name should be based on the id of the pet');
     expect(wrapper.find('h2').text()).toEqual('Fido');
