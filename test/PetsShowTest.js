2febdf2c4a9221d9cf96b5a785e59103e42250e5
Luke Ghenco <lukeghenco@gmail.com>
2017-06-13 13:45:28 -0400

    Update tests to use arrow functions

--- test/PetsShowTest.js
+++ test/PetsShowTest.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import expect from 'expect';
-import {mount} from 'enzyme';
+import { mount } from 'enzyme';
 
 import FakeProvider from './FakeProvider';
 import PetsShow from '../src/containers/PetsShow';
 
-describe('PetsShow', function(){
+describe('PetsShow', () => {
 
-  it('renders the pets show component', function(){
-    const wrapper = mount(<FakeProvider>< PetsShow routeParams={{id: 1}} /></FakeProvider>);
+  it('renders the pets show component', () => {
+    const matchParams = { params: { movieId: 1 } }
+    const wrapper = mount(<FakeProvider>< PetsShow match={matchParams} /></FakeProvider>);
     expect(wrapper.find('h2').length).toEqual(1, 'Pets show should have an h2 with the pets name');
     expect(wrapper.find('h2').text()).toEqual('Grover');
   })
 
-  it('finds the pet by the route ID', function(){
-    const wrapper = mount(<FakeProvider>< PetsShow routeParams={{id: 2}} /></FakeProvider>);
+  it('finds the pet by the route ID', () => {
+    const matchParams = { params: { movieId: 2 } }
+    const wrapper = mount(<FakeProvider>< PetsShow match={matchParams} /></FakeProvider>);
     expect(wrapper.find('h2').length).toEqual(1, 'The name should be based on the id of the pet');
     expect(wrapper.find('h2').text()).toEqual('Fido');
   })
-
 })
