713308edb1eb14ce8f8587775c3c8d26e704a064
Luke Ghenco <lukeghenco@gmail.com>
2017-06-13 14:01:21 -0400

    Add intital code for App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import PetsPage from '../containers/PetsPage';
 
-export default (props) => {
-  return (
-    <Router>
-      <div>
-        <Link to="pets">See All The Pets!</Link>
+const App = (props) => 
+  <Router>
+    <div>
+      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+        <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Pets!</NavLink>
+        <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Pet</NavLink>
       </div>
-    </Router>
-  )
-};
+      <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
+      <Route path="/pets" component={PetsPage} />
+    </div>
+  </Router>;
+
+export default App;
